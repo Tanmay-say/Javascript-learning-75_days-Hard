df6a6f033822f1a5f0c0bfc8a7ed70dbb433aa9d
tanmaysayare <tanmaysayare12345@gmail.com>
2024-01-17 01:41:01 +0530

    Array used

--- Day1/print.js
+++ Day1/print.js
@@ -6,3 +6,5 @@ console.log("Tanmay age is :" ,Tanmay_age,"And Tanmay Have ",friend," friends");
 let dateandtime = new Date();
 console.log("Current date and time is ",dateandtime);
 
+let numbers = [1, 2, 3, 4, 5];
+console.log(numbers);
