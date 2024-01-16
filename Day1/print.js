8727ec3503a2eb5d1d42d309e917b17d3a019780
tanmaysayare <tanmaysayare12345@gmail.com>
2024-01-17 01:26:02 +0530

    Current Date and Time added

--- Day1/print.js
+++ Day1/print.js
@@ -2,3 +2,6 @@ console.log("Tanmay is Here \n\n");
 let Tanmay_age = 20;
 const friend = 3;
 console.log("Tanmay age is :" ,Tanmay_age,"And Tanmay Have ",friend," friends");
+
+let dateandtime = new Date();
+console.log("Current date and time is ",dateandtime);
