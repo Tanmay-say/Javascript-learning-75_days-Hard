4ba22a608fa35004ae03c73c8f8370b21255a9b7
tanmaysayare <tanmaysayare12345@gmail.com>
2024-01-17 01:18:39 +0530

    variable tanmay_age and constant friend used

--- Day1/print.js
+++ Day1/print.js
@@ -1,4 +1,4 @@
 console.log("Tanmay is Here \n\n");
-
 let Tanmay_age = 20;
-console.log(Tanmay_age);
+const friend = 3;
+console.log("Tanmay age is :" ,Tanmay_age,"And Tanmay Have ",friend," friends");
