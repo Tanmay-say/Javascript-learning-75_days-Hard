4b3d4212ab8dff4f5f57105c13b0f468ade8076d
tanmaysayare <tanmaysayare12345@gmail.com>
2024-01-17 01:32:55 +0530

    Arithematic Operators added

--- Day1/sum.js
+++ Day1/sum.js
@@ -6,3 +6,17 @@ console.log("Sub is :" ,x-y);
 console.log("Mul is :" ,x*y);
 console.log("Div is :" ,x/y);
 console.log("Mod is :" ,x%y);
+
+x = 5;
+y = 2;
+let sum = x + y;
+let difference = x - y;
+let product = x * y;
+let quotient = x / y;
+
+console.log("\n\nSum is : ",sum);
+console.log("Difference is : ",difference);
+console.log("product is : ",product);
+console.log("Quotient is : ",quotient);
+
+
