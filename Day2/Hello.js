d03886254141f740ceda36f7d53fd939bcd6835e
tanmaysayare <tanmaysayare12345@gmail.com>
2024-01-25 14:52:54 +0530

    var is used

--- Day2/Hello.js
+++ Day2/Hello.js
@@ -1,1 +1,19 @@
 console.log("hello");
+var a = 10 ;
+console.log(a);
+let b = 10 ;
+console.log(b);
+c = a+b;
+console.log(c);
+
+console.log("=====================================================================");
+var a =  30 ;
+console.log(a);
+a = "hello";
+console.log(a);
+if(a == 30)
+{
+  console.log(a);
+}
+else
+  console.log("Hello is here");
