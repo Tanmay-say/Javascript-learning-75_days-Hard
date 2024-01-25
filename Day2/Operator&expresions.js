6e4a0acba4e2eda684255fda970e6005ad577c89
tanmaysayare <tanmaysayare12345@gmail.com>
2024-01-25 17:27:47 +0530

    Arithemetic opeator added

---
+++ Day2/Operator&expresions.js
@@ -0,0 +1,28 @@
+//                                         '"Arithemetic operators"'
+// + : Addition
+// - : Substractions
+// / : Division
+// * : Multiplications
+// ** : Power
+// % : Modules
+// ++ : Increment 
+// -- : Decrement 
+
+console.log("Operators in js : \n");
+let a = 2 , b = 2 ;
+console.log("a + b is : ",a+b);
+console.log("a - b is : ",a-b);
+console.log("a * b is : ",a*b);
+console.log("a / b is : ",a/b);
+console.log("a % b is : ",a%b);
+console.log("a ** b is : ",a**b);
+console.log("a++ is : ", (a++) ); //post - increment
+console.log(a);
+console.log("b-- is : ",(b--));
+console.log(b);
+console.log("++a is : ", (++a) ); //pre-incement
+console.log("--b is : ", (--b) );
+
+
+
+
