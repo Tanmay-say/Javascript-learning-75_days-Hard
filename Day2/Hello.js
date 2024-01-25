41a6a6b5574fb3b110f69c94f07cc717185a7380
tanmaysayare <tanmaysayare12345@gmail.com>
2024-01-25 15:17:01 +0530

    Typeof used

--- Day2/Hello.js
+++ Day2/Hello.js
@@ -1,5 +1,5 @@
 let a = "abs";
 a = "abx";
 const b = 3.14;
-b = 312;
+
 console.log(b);
--- Day2/Primitive_datatype.js
+++ Day2/Primitive_datatype.js
@@ -7,3 +7,13 @@
 //Bigint
 //Undefined
 
+let a = null;
+let b = 345;
+let c = true;
+let d = BigInt("567") + BigInt("3");
+let e = "Tanmay";
+let f = Symbol("I am a symbol");
+let g = undefined;
+console.log(a ,b ,c ,d ,e ,f ,g );
+
+console.log(typeof d);
