ff077babb3554efe29b1d928ed4fb118134a1219
tanmaysayare <tanmaysayare12345@gmail.com>
2024-01-25 14:59:46 +0530

    Let and var difference

--- Day2/Hello.js
+++ Day2/Hello.js
@@ -1,19 +1,20 @@
-console.log("hello");
-var a = 10 ;
-console.log(a);
-let b = 10 ;
-console.log(b);
-c = a+b;
-console.log(c);
-
-console.log("=====================================================================");
-var a =  30 ;
-console.log(a);
-a = "hello";
+const a =  30 ;
+var b = null;
+var c = undefined ;
+var str = "Hello"
+{
+    let str = 'this';
+    console.log(str);
+}
 console.log(a);
 if(a == 30)
 {
   console.log(a);
+  console.log(b);
+  console.log(c);
+  console.log(str);
+
 }
 else
   console.log("Hello is here");
+
