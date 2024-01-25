205b5c64cc4d566576871474678d788acff6c193
tanmaysayare <tanmaysayare12345@gmail.com>
2024-01-25 15:31:43 +0530

    Q1 completed

---
+++ Day2/practise_set.js
@@ -0,0 +1,10 @@
+//Q1 create a variable of type string and try to add a number to it .
+
+let a = "tanmay ";
+let b = 10;
+console.log(a + b);
+let z = 20 ;
+let y = "is here ";
+console.log(a + z);
+console.log(a + y);
+console.log(z + b);
