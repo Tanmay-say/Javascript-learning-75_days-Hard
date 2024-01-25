6be3e97079110d63d04c467ffc6e3c7c66346904
tanmaysayare <tanmaysayare12345@gmail.com>
2024-01-25 17:38:54 +0530

    Q1 solved

--- Day2/Operator&expresions.js
+++ Day2/Operator&expresions.js
@@ -26,3 +26,24 @@ console.log("--b is : ", (--b) );
 
 
 
+//Q1 Problem to solve  :- 
+let z = 100 ;
+console.log("z++ : ", (z++) );    //101         //print : 100  
+console.log("z++ : ", (z++) );    //102         //print : 101
+console.log("++z : ", (++z) );    //103         //print : 103
+console.log("++z : ", (++z) );    //104         //print : 104
+console.log("z++ : ", (z++) );    //105         //print : 104
+console.log("++z : ", (++z) );    //106         //print : 106 
+console.log("++z : ", (++z) );    //107         //print : 107 
+console.log("++z : ", (++z) );    //108         //print : 108
+console.log("z++ : ", (z++) );    //109         //print : 108
+console.log("++z : ", (++z) );    //110         //print : 110
+console.log("z++ : ", (z++) );    //111         //print : 110
+
+console.log("z : ",z);
+
+
+
+
+
+
