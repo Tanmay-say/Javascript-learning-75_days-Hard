ff9445c37188aa9a0eb3da7ff91e464d1ff96d32
tanmaysayare <tanmaysayare12345@gmail.com>
2024-01-25 15:06:04 +0530

    Const used

--- Day2/Hello.js
+++ Day2/Hello.js
@@ -1,20 +1,5 @@
-const a =  30 ;
-var b = null;
-var c = undefined ;
-var str = "Hello"
-{
-    let str = 'this';
-    console.log(str);
-}
-console.log(a);
-if(a == 30)
-{
-  console.log(a);
-  console.log(b);
-  console.log(c);
-  console.log(str);
-
-}
-else
-  console.log("Hello is here");
-
+let a = "abs";
+a = "abx";
+const b = 3.14;
+b = 312;
+console.log(b);
