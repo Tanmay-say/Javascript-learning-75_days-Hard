2f98285eb1bf69d255252c8fd2cfe97735b8fd38
tanmaysayare <tanmaysayare12345@gmail.com>
2024-01-25 23:00:14 +0530

    Conditional expressions added

---
+++ Day2/Conditional_expressions.js
@@ -0,0 +1,16 @@
+//                       CONDITIONAL EXPRESS
+/*
+IF
+IF...ELSE
+IF...ELSE IF .....ELSE 
+*/
+
+
+let a = 2.02;
+a = parseInt(a);
+console.log(typeof a);
+
+if (a >= 0)
+{
+    console.log("false");
+}
