a23deb38d7ce737ebe7c9ec880d23599e336823e
tanmaysayare <tanmaysayare12345@gmail.com>
2024-01-26 22:26:00 +0530

    Calculator create by switch case and while loop accurate exit

--- Day3/calculator.js
+++ Day3/calculator.js
@@ -1,15 +1,9 @@
-let i = 0 ;
+let i = 1 ;
 i = Number.parseInt(i);
 while(true){
-i++;
-console.log("1. For Addition\n");
-console.log("2. For Substraction\n");
-console.log("3. For Multiplication\n");
-console.log("4. For Division\n");
-console.log("5. For Modules\n");
-console.log("6. For Exit\n");
-let choice = prompt("Enter your choice\n");
+let choice = prompt("Enter your choice\n1. For Addition\n2. For Substraction\n3. For Multiplication\n4. For Division\n5. For Modules\n6. For Exit\n");
 choice = Number.parseInt(choice);
+if(choice < 6){
 let a = prompt("Enter value of A\n");
 let b = prompt("Enter value of B\n");
 switch(choice)
@@ -29,20 +23,19 @@ switch(choice)
     case 5 : {
         alert(Number.parseInt(a%b));
     }break;
-    case 6 : {
-        break;
-    }
     default : alert("Something went wrong");
     break;
 }
-console.log("Do you want to continue ",i,"\n");
-console.log("1. To continue \n");
-console.log("2. To exit \n");
-let conti = prompt("Enter 1 or 2 ");
+let conti = prompt("Do you want to continue \n1. To continue \n2. To exit \n");
 if(conti == 1)
 {
-    let z = true;
+    alert("!!! Your are conitnuing !!!");
 }
-else{
-break;}
+else{break;}
+
+}
+else if(choice == 6)
+{break;}
+else{alert("!!! Try Again !!!");}
+
 }
