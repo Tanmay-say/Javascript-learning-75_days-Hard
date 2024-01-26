c7d404154586bbb8b0d9700c6d3ca5af0f54f06a
tanmaysayare <tanmaysayare12345@gmail.com>
2024-01-26 20:46:57 +0530

    Voter system

--- Day2/Conditional_expressions.js
+++ Day2/Conditional_expressions.js
@@ -9,7 +9,8 @@ IF...ELSE IF .....ELSE
 let a = 97;
 a = parseInt(a);
 console.log(typeof a);
-
+if(a > 0)
+{
 if (a <= 9)
 {
     console.log("you are a kid");
@@ -24,3 +25,6 @@ else if(18 <= a && a < 90)
 }
 else
 console.log("Something went wrong");
+}
+else
+console.log("Age can't be negative");
