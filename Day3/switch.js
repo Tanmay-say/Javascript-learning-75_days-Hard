6df216c4b1b7e76a92ad8c832e07ab703d2504a8
tanmaysayare <tanmaysayare12345@gmail.com>
2024-01-26 21:14:30 +0530

    Added switch case

---
+++ Day3/switch.js
@@ -0,0 +1,14 @@
+let a = 1;
+switch(a)
+{
+    case 1 : console.log("Are are not elligible");
+    break;
+    case 2 : console.log("Are are not elligible");
+    break;
+    case 3 : console.log("Are are elligible");
+    break;
+    case 4 : console.log("Are are elligible");
+    break;
+    default : console.log("Somehing went wrong");
+    break;
+} 
