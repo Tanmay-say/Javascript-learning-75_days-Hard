89de970a19e0d7143e9b55f87ae05bfc66e74e5c
tanmaysayare <tanmaysayare12345@gmail.com>
2024-01-26 22:06:59 +0530

    Calculator create by switch case and Do-while loop

--- Day3/calculator.js
+++ Day3/calculator.js
@@ -1,29 +1,49 @@
+let i = 0 ;
+i = Number.parseInt(i);
+do{
+i++;
 console.log("1. For Addition\n");
 console.log("2. For Substraction\n");
 console.log("3. For Multiplication\n");
 console.log("4. For Division\n");
 console.log("5. For Modules\n");
+console.log("6. For Exit\n");
 let choice = prompt("Enter your choice\n");
 choice = Number.parseInt(choice);
-let a = prompt("Enter value of a\n");
-let b = prompt("Enter value of b\n");
+let a = prompt("Enter value of A\n");
+let b = prompt("Enter value of B\n");
 switch(choice)
 {
     case 1 : {
-        console.log(Number.parseInt(Number.parseInt(a)+Number.parseInt(b)));
+        alert(Number.parseInt(Number.parseInt(a)+Number.parseInt(b)));
     }break;
     case 2 : {
-        console.log(Number.parseInt(a-b));
+        alert(Number.parseInt(a-b));
     }break;
     case 3 : {
-        console.log(Number.parseInt(a*b));
+        alert(Number.parseInt(a*b));
     }break;
     case 4 : {
-        console.log(Number.parseInt(a/b));
+        alert(Number.parseInt(a/b));
     }break;
     case 5 : {
-        console.log(Number.parseInt(a%b));
+        alert(Number.parseInt(a%b));
     }break;
-    default : console.log("Something went wrong");
+    case 6 : {
+        break;
+    }
+    default : alert("Something went wrong");
     break;
 }
+console.log("Do you want to continue ",i,"\n");
+console.log("1. To continue \n");
+console.log("2. To exit \n");
+let conti = prompt("Enter 1 or 2 ");
+if(conti == 1)
+{
+    let z = true;
+}
+else
+break;
+
+}while(true);
