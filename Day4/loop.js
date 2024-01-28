816b6534ce3d91585302ecf30032ab27a2248c1c
tanmaysayare <tanmaysayare12345@gmail.com>
2024-01-28 16:27:13 +0530

    Natural program sum

---
+++ Day4/loop.js
@@ -0,0 +1,16 @@
+let b = 1;
+for (let i = 0 ; i <= 5  ; i++)
+{
+    i += b;
+    console.log(i);
+}
+
+//Program to add n natural numbers
+
+n = 10 ;
+for(i = 1 ; i <=n ; i++)
+{
+    a = i + 1;
+    sum = a + i;
+}
+console.log(sum);
